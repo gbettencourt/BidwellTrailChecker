790af973e61a4a258006716adca3c90715c26dd3
Gary Bettencourt <gary.bettencourt@build.com>
2020-08-19 20:24:58 -0700

    prettier

--- client/src/App/pages/SignUp.js
+++ client/src/App/pages/SignUp.js
@@ -11,153 +11,168 @@ import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 
-const styles = theme => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+const styles = (theme) => ({
+	paper: {
+		marginTop: theme.spacing(8),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing(3),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
 });
 
 class SignUp extends Component {
+	constructor(props) {
+		super(props);
+		this.handleRegisterClick = this.handleRegisterClick.bind(this);
+		this.handleEmailChange = this.handleEmailChange.bind(this);
+		this.state = {
+			isRegistered: false,
+			isEmailValid: true,
+			email: '',
+			emailHelperText: '',
+			regError: '',
+			trailStatus: '',
+			lastCheckTime: '',
+		};
+	}
 
-  constructor(props) {
-    super(props);
-    this.handleRegisterClick = this.handleRegisterClick.bind(this);
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.state = {
-      isRegistered: false,
-      isEmailValid: true,
-      email: '',
-      emailHelperText: '',
-      regError: '',
-      trailStatus: '',
-      lastCheckTime: ''
-    };
-  }
+	componentDidMount() {
+		fetch('/api/trailstatus')
+			.then((res) => res.json())
+			.then((status) =>
+				this.setState({
+					trailStatus: status.trailStatus,
+					lastCheckTime: status.lastCheck,
+				})
+			);
+	}
 
-  componentDidMount() {
-    fetch('/api/trailstatus')
-      .then(res => res.json())
-      .then(status => this.setState({ trailStatus: status.trailStatus, lastCheckTime: status.lastCheck }));
-  }
+	handleRegisterClick() {
+		let isValidEmail = document.getElementById('email').validity.valid;
+		if (!isValidEmail) {
+			this.setState({
+				isEmailValid: false,
+				emailHelperText: 'Please enter a valid email',
+			});
+			return;
+		}
+		let postData = { email: this.state.email };
+		fetch('/api/registeremail', {
+			method: 'post',
+			body: JSON.stringify(postData),
+			headers: { 'Content-Type': 'application/json' },
+		})
+			.then((res) => res.json())
+			.then((result) => {
+				if (result.success) {
+					this.setState({ isRegistered: true });
+				} else {
+					this.setState({ regError: result.error });
+				}
+			});
+	}
 
-  handleRegisterClick() {
-    let isValidEmail = document.getElementById('email').validity.valid;
-    if (!isValidEmail) {
-      this.setState({ isEmailValid: false, emailHelperText: "Please enter a valid email" });
-      return;
-    }
-    let postData = { "email": this.state.email };
-    fetch('/api/registeremail', {
-      method: 'post',
-      body: JSON.stringify(postData),
-      headers: { "Content-Type": "application/json" }
-    })
-      .then(res => res.json())
-      .then(result => {
-        if (result.success) {
-          this.setState({ isRegistered: true });
-        } else {
-          this.setState({ regError: result.error });
-        }
-      });
-  }
+	handleEmailChange(event) {
+		this.setState({ email: event.target.value });
+	}
 
-  handleEmailChange(event) {
-    this.setState({ email: event.target.value });
-  }
+	render() {
+		const { classes } = this.props;
+		const isRegistered = this.state.isRegistered;
+		const trailStatusStyle = {
+			color: this.state.trailStatus === 'Open' ? 'green' : 'red',
+		};
 
-  render() {
-    const { classes } = this.props;
-    const isRegistered = this.state.isRegistered;
-    const trailStatusStyle = {
-      color: this.state.trailStatus === "Open" ? "green" : "red"
-    };
-
-    return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <BikeIcon />
-          </Avatar>
-          {isRegistered ? (
-            <Typography component="h1" variant="h5">
-              Your registration was successful!
-            </Typography>
-          ) : (
-              <div>
-                <Box mt={2}></Box>
-                <Typography component="h1" variant="h5">
-                  Current trail status: <span style={trailStatusStyle}>{this.state.trailStatus}</span>
-                </Typography>
-                <Typography component="h8" variant="h8">
-                  Last checked: <span>{new Date(this.state.lastCheckTime).toLocaleString()}</span>
-                </Typography>
-                <Box mt={5}></Box>
-                <Typography component="h1" variant="h5">
-                  Enter your email address to get notifications when the Upper Bidwell Park trail status changes:
-                </Typography>
-                <form className={classes.form} noValidate>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                      <TextField
-                        variant="outlined"
-                        required
-                        fullWidth
-                        id="email"
-                        type="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        value={this.state.email}
-                        helperText={this.state.emailHelperText}
-                        error={!this.state.isEmailValid}
-                        onChange={this.handleEmailChange}
-                      />
-                    </Grid>
-                  </Grid>
-                  <Button
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    className={classes.submit}
-                    onClick={this.handleRegisterClick}
-                  >
-                    Sign Up
-                  </Button>
-                </form>
-              </div>
-            )}
-        </div>
-        <Box mt={2}>
-
-        </Box>
-        <Box>
-          <Link href='Unsubscribe'>
-            Unsubscribe
-        </Link>
-        </Box>
-        <Box>
-          <Link href='http://www.chico.ca.us/general_services_department/park_division/bidwell_park.asp'>
-            Park Trail Page
-        </Link>
-        </Box>
-      </Container >
-    );
-  }
+		return (
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<div className={classes.paper}>
+					<Avatar className={classes.avatar}>
+						<BikeIcon />
+					</Avatar>
+					{isRegistered ? (
+						<Typography component="h1" variant="h5">
+							Your registration was successful!
+						</Typography>
+					) : (
+						<div>
+							<Box mt={2}></Box>
+							<Typography component="h1" variant="h5">
+								Current trail status:{' '}
+								<span style={trailStatusStyle}>
+									{this.state.trailStatus}
+								</span>
+							</Typography>
+							<Typography component="h8" variant="h8">
+								Last checked:{' '}
+								<span>
+									{new Date(
+										this.state.lastCheckTime
+									).toLocaleString()}
+								</span>
+							</Typography>
+							<Box mt={5}></Box>
+							<Typography component="h1" variant="h5">
+								Enter your email address to get notifications
+								when the Upper Bidwell Park trail status
+								changes:
+							</Typography>
+							<form className={classes.form} noValidate>
+								<Grid container spacing={2}>
+									<Grid item xs={12}>
+										<TextField
+											variant="outlined"
+											required
+											fullWidth
+											id="email"
+											type="email"
+											label="Email Address"
+											name="email"
+											autoComplete="email"
+											value={this.state.email}
+											helperText={
+												this.state.emailHelperText
+											}
+											error={!this.state.isEmailValid}
+											onChange={this.handleEmailChange}
+										/>
+									</Grid>
+								</Grid>
+								<Button
+									fullWidth
+									variant="contained"
+									color="primary"
+									className={classes.submit}
+									onClick={this.handleRegisterClick}
+								>
+									Sign Up
+								</Button>
+							</form>
+						</div>
+					)}
+				</div>
+				<Box mt={2}></Box>
+				<Box>
+					<Link href="Unsubscribe">Unsubscribe</Link>
+				</Box>
+				<Box>
+					<Link href="https://www.chico.ca.us/park-trails">
+						Park Trail Page
+					</Link>
+				</Box>
+			</Container>
+		);
+	}
 }
 export default withStyles(styles)(SignUp);
--- index.js
+++ index.js
@@ -1,111 +1,102 @@
 const express = require('express');
 var winston = require('winston'),
-    expressWinston = require('express-winston');
-const {
-    check,
-    validationResult
-} = require('express-validator');
+	expressWinston = require('express-winston');
+const { check, validationResult } = require('express-validator');
 const path = require('path');
 const Checker = require('./checker');
 const DbUtil = require('./db');
 const app = express();
 app.use(express.json());
 
-app.use(expressWinston.logger({
-    transports: [
-        new winston.transports.Console()
-    ],
-    format: winston.format.combine(
-        winston.format.colorize({ all: true }),
-        winston.format.timestamp({
-            format: 'MM-DD-YYYY HH:mm:ss'
-        }),
-        winston.format.printf(
-            (info) => {
-                return `${info.timestamp}: ${info.message}`;
-            })
-    )
-}));
+app.use(
+	expressWinston.logger({
+		transports: [new winston.transports.Console()],
+		format: winston.format.combine(
+			winston.format.colorize({ all: true }),
+			winston.format.timestamp({
+				format: 'MM-DD-YYYY HH:mm:ss'
+			}),
+			winston.format.printf(info => {
+				return `${info.timestamp}: ${info.message}`;
+			})
+		)
+	})
+);
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('/api/users', async (req, res) => {
-    let list = [];
-    if (req.connection.localAddress === req.connection.remoteAddress) {
-        list = await dbUtil.fetchEmails();
-    }
-    res.json(list);
+	let list = [];
+	if (req.connection.localAddress === req.connection.remoteAddress) {
+		list = await dbUtil.fetchEmails();
+	}
+	res.json(list);
 });
 
 app.get('/api/trailstatus', (req, res) => {
-    res.json(checker.currentStatus());
+	res.json(checker.currentStatus());
 });
 
-app.post('/api/registeremail', [
-    check('email').isEmail(),
-], async (req, res) => {
+app.post('/api/registeremail', [check('email').isEmail()], async (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		console.log('registeremail invalid email=' + req.body);
+		res.send({
+			success: false,
+			error: 'invalid email'
+		});
+		return;
+	}
+	let email = req.body.email;
+	console.log('register new email=' + email);
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        console.log('registeremail invalid email=' + req.body);
-        res.send({
-            success: false,
-            error: 'invalid email'
-        });
-        return;
-    }
-    let email = req.body.email;
-    console.log('register new email=' + email);
+	let list = await dbUtil.fetchEmails();
+	if (list.includes(email)) {
+		res.send({
+			success: false,
+			error: 'email already registered'
+		});
+		return;
+	}
+	await dbUtil.addEmail(email);
 
-    let list = await dbUtil.fetchEmails();
-    if (list.includes(email)) {
-        res.send({
-            success: false,
-            error: 'email already registered'
-        });
-        return;
-    }
-    await dbUtil.addEmail(email);
+	//send confirmation emails
+	try {
+		checker.sendNewUserNotification(email);
+		checker.sendRegEmail(email);
+	} catch (e) {
+		console.log('error sending new user email', e);
+	}
 
-    //send confirmation emails
-    try {
-        checker.sendNewUserNotification(email);
-        checker.sendRegEmail(email);
-    } catch (e) {
-        console.log('error sending new user email', e);
-    }
-
-    res.send({
-        success: true
-    });
+	res.send({
+		success: true
+	});
 });
 
-app.post('/api/rmemail', [
-    check('email').isEmail(),
-], async (req, res) => {
-    console.log(req.body);
+app.post('/api/rmemail', [check('email').isEmail()], async (req, res) => {
+	console.log(req.body);
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        res.send({
-            success: false,
-            error: 'invalid email'
-        });
-        return;
-    }
-    let email = req.body.email;
-    console.log('removing email=' + email);
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		res.send({
+			success: false,
+			error: 'invalid email'
+		});
+		return;
+	}
+	let email = req.body.email;
+	console.log('removing email=' + email);
 
-    await dbUtil.removeEmail(email);
-    res.send({
-        success: true
-    });
+	await dbUtil.removeEmail(email);
+	res.send({
+		success: true
+	});
 });
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+	res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 //init trail checker
