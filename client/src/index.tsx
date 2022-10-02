8b72b7ba3cc9466c301d5af92fd7380ef13a24ff
Gary Bettencourt <gary.bettencourt@build.com>
2022-10-01 17:55:18 -0700

    update to React 18

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,14 @@
 import React from "react";
-import { render } from "react-dom";
+import * as ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App/App";
 
-render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+if (!container) throw new Error("Failed to find the root element");
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
